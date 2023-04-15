f1e89cec142fd79997c148f42a1e9f7adc68b8e6
AidilDaniel <aidilvendetta@gmail.com>
2023-04-16 00:30:29 +0800

    Main Page Niken

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Home Page</title>
+  <link rel="stylesheet" href="/css/index.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <?php include '/xampp/htdocs/Niken/public/header/header.php'; ?>
+
+  <header>
+
+    <div class="header-content">
+      <h2>INNOVATIVE MIND</h2>
+      <div class="line"></div>
+      <h1>CHALLENGE THE IMPOSSIBLE</h1>
+      <a href="#" class="ctn">Explore</a>
+    </div>
+    
+  </header>
+
+</body>
+
+</html>
